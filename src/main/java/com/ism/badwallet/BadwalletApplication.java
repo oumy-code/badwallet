a9aca06ea92adc8aadc068cf0d867b6261240720
oumy-code <princesselebou12@gmail.com>
2026-06-29 09:15:08 +0000

    Initial commit - Base du projet

---
+++ src/main/java/com/ism/badwallet/BadwalletApplication.java
@@ -0,0 +1,13 @@
+package com.ism.badwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadwalletApplication.class, args);
+	}
+
+}
